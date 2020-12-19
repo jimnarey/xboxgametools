9aaef42d948c7f01006e8ae02dd22f0bf1fad66a
Jim Narey <jimnarey@me.com>
2020-12-19 15:33:20 +0000

    Simplify generation of all dump/ro codes

--- sortroms.py
+++ sortroms.py
@@ -28,13 +28,20 @@ class ArchiveEntry:
 
     def __init__(self, entry_name):
         self.entry_name = entry_name
-        self.region_other_codes = []
+        self.ro_codes = []
         self.dump_codes = []
         self._populate_codes()
 
     def _populate_codes(self):
-        self.region_other_codes = re.findall("\((.*?)\)", self.entry_name)
+        self.ro_codes = re.findall("\((.*?)\)", self.entry_name)
         self.dump_codes = re.findall("\[(.*?)\]", self.entry_name)
+
+    def all_codes(self, code_type):
+        if code_type == 'dump':
+            return self.dump_codes
+        elif code_type == 'ro':
+            return self.ro_codes
+        return None
 
 
 class ArchiveFile:
@@ -42,9 +49,9 @@ class ArchiveFile:
     def __init__(self, file_path):
         self.file_path = file_path
         self.all_entries = []
-        # self.sorted_entries = []
         self.ok_dumps = []
-        self.ok_dump_and_region = []
+        self.ok_ro = []
+        self.matches = []
 
     def populate_entries(self):
         try:
@@ -55,35 +62,40 @@ class ArchiveFile:
             return False
         return True
 
-    # def match_dump_code(self, code_regex):
-    #     for entry in self.all_entries:
-    #         for code in entry.dump_codes:
-    #             if re.match(code_regex, code):
-    #                 return entry
-
-    # def cut_by_ro_codes(self, bad_codes):
-    #     for entry in self.sorted_entries:
-    #         for code in bad_codes:
-    #             if code in entry.region_other_codes:
-    #                 self.sorted_entries.remove(entry)
-
-    def get_ok_dumps(self, ok_dump_regexes):
+    def _get_ok_dumps(self, ok_dump_regexes):
         for entry in self.all_entries:
             for code in entry.dump_codes:
                 if any(re.match(regex, code) for regex in ok_dump_regexes):
                     self.ok_dumps.append(entry)
-                    # print(entry.entry_name)
             if not entry.dump_codes:
                 self.ok_dumps.append(entry)
 
-    def get_ok_dumps_regions(self, ok_ro_list):
-        for entry in self.ok_dumps:
+    def _get_ok_ro(self, ok_ro_list):
+        for entry in self.all_entries:
             for code in ok_ro_list:
-                if code in entry.region_other_codes:
-                    self.ok_dump_and_region.append(entry)
- 
+                if code in entry.ro_codes:
+                    self.ok_ro.append(entry)
+
+    def _cross_ref_matches(self):
+        if self.ok_dumps and self.ok_ro:
+            self.matches = [entry for entry in self.ok_dumps if entry in self.ok_ro]
+
+    def get_matches(self, ok_dump_regexes, ok_ro_list):
+        self._get_ok_dumps(ok_dump_regexes)
+        self._get_ok_ro(ok_ro_list)
+        self._cross_ref_matches
+
     def get_num_matches(self):
-        return len(self.ok_dump_and_region)
+        return len(self.matches)
+
+    # Use set
+    def all_codes(self, code_type):
+        codes = []
+        for entry in self.all_entries:
+            for code in entry.all_codes(code_type):
+                if code not in codes:
+                    codes.append(code)
+        return codes
 
 
 class RomRootFolder:
@@ -110,33 +122,39 @@ class RomRootFolder:
             else:
                 self.invalid_file_paths.append(file_path)
 
-    def all_dump_codes(self):
-        dump_codes = []
-        for archive in self.valid_archives:
-            for entry in archive.all_entries:
-                for code in entry.dump_codes:
-                    if code not in dump_codes:
-                        dump_codes.append(code)
-        return dump_codes
-
-    def all_ro_codes(self):
-        ro_codes = []
-        for archive in self.valid_archives:
-            for entry in archive.all_entries:
-                for code in entry.region_other_codes:
-                    if code not in ro_codes:
-                        ro_codes.append(code)
-        return ro_codes
-
-    def sort(self):
+    def all_codes(self, code_type):
+        codes = []
         for arch in self.valid_archives:
-            arch.get_ok_dumps(ok_dump_regexes)
-            arch.get_ok_dumps_regions(english_language_codes)
+            for code in arch.all_codes(code_type):
+                if code not in codes:
+                    codes.append(code)
+        return codes
+
+    # def all_dump_codes(self):
+    #     dump_codes = []
+    #     for archive in self.valid_archives:
+    #         for entry in archive.all_entries:
+    #             for code in entry.dump_codes:
+    #                 if code not in dump_codes:
+    #                     dump_codes.append(code)
+    #     return dump_codes
+
+    # def all_ro_codes(self):
+    #     ro_codes = []
+    #     for archive in self.valid_archives:
+    #         for entry in archive.all_entries:
+    #             for code in entry.ro_codes:
+    #                 if code not in ro_codes:
+    #                     ro_codes.append(code)
+    #     return ro_codes
+
+    def sort(self, ok_dump_regexes, ok_ro_codes):
+        for arch in self.valid_archives:
+            arch.get_matches(ok_dump_regexes, ok_dump_regexes)
 
     def write_log(self):
         output = []
         for arch in self.valid_archives:
-            # output.append(arch.file_path + str(arch.get_num_matches()) + " - " + '\n')
             output.append("{file} : {matches}\n".format(file=arch.file_path, matches=str(arch.get_num_matches())))
         with open(self.log_path, 'w') as log_file:
             log_file.writelines(output)
@@ -148,7 +166,7 @@ start_path_linux_vm = '/media/sf_G_DRIVE/Romsets/Genesis Full Set'
 log_path_linux_vm = '/media/sf_G_DRIVE/Romsets/genesis_log.txt'
 
 root = RomRootFolder(start_path_linux_vm, log_path_linux_vm)
-root.sort()
+# root.sort()
 root.write_log()
 
 # for arch in root.valid_archives:
